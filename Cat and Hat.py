803dc8516fa5bdab0977d5d7a963dd6b3149b5ff
Danish karim <146133341+danisharain12@users.noreply.github.com>
2025-03-11 15:54:41 +0500

    Update Cat and Hat.py

--- Cat and Hat.py
+++ Cat and Hat.py
@@ -1,5 +1,7 @@
-#You are given a string str, you need to return True if  the words "cat" and "hat" appear same number of times in str, otherwise return False.
-#Note: str contains only lowercase English alphabets
+# Return True if  the words "cat" and "hat" appear same number of times in str, otherwise return False.
+# Note: str contains only lowercase English alphabets
+----------------------------------------------------------------------------------------------------------------------------------------------
+
 def cat_hat(str):
     if str.count('cat') == str.count('hat'):
         return True
